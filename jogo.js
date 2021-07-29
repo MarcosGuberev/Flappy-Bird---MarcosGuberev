baed4582c63563bb999b215a58af0fcb9e90449e
MarcosGuberev <marcosguberev@gmail.com>
2021-07-29 16:30:07 -0300

    Subindo o passaro, colisao e reinicio do jogo ao colidir e som

--- jogo.js
+++ jogo.js
@@ -1,5 +1,8 @@
 console.log('Marcos Guberev Flappy Bird');
 
+const som_Hit = new Audio(); 
+som_Hit.src ='./efeitos/hit.wav'; 
+
 const sprites = new Image();
 sprites.src ='sprites.png';
 
@@ -64,31 +67,62 @@ const planoDeFundo = {
     }
   }
 
-  const flappyBird = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 33,
-    altura: 24,
-    x: 10,
-    y: 50,
-    gravidade: 0.25,
-    velocidade: 0, 
-    //algum comentario 
-  
-    atualiza(){
-      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade, 
-      flappyBird.y = flappyBird.y + flappyBird.velocidade;
-    },
-    desenha() {
-      contexto.drawImage(
-        sprites,
-        flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-        flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-        flappyBird.x, flappyBird.y,
-        flappyBird.largura, flappyBird.altura,
-      );
+  function fazColisao(flappyBird, chao){  
+    const flappyBirdY = flappyBird.y + flappyBird.altura; 
+    const chaoY = chao.y; 
+
+    if (flappyBirdY >= chaoY){
+      return true; 
     }
+    return false; 
   }
+  
+  function criaFlappyBird(){
+    const flappyBird = {
+      spriteX: 0,
+      spriteY: 0,
+      largura: 33,
+      altura: 24,
+      x: 10,
+      y: 50,
+      pulo:  4.6, 
+      pula(){
+        console.log('antes', flappyBird.velocidade); 
+        flappyBird.velocidade = - flappyBird.pulo;
+        console.log('depois' , flappyBird.velocidade); 
+      }, 
+      gravidade: 0.25,
+      velocidade: 0, 
+      //algum comentario 
+    
+      atualiza(){
+        if (fazColisao(flappyBird, chao)){
+          console.log('fez colisão'); 
+          som_Hit.play();
+          setTimeout(() => {
+            mudaParaTela(telas.INICIO); 
+          }, 500);
+          
+          return; 
+        }
+  
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade, 
+        flappyBird.y = flappyBird.y + flappyBird.velocidade;
+      },
+      desenha() {
+        contexto.drawImage(
+          sprites,
+          flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+          flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+          flappyBird.x, flappyBird.y,
+          flappyBird.largura, flappyBird.altura,
+        );
+      },
+    }
+    return flappyBird; 
+  }
+
+ 
 
   const mensagemGetReady = {
     spriteX: 133,
@@ -109,19 +143,29 @@ const planoDeFundo = {
 
   }
 
+  const globais = {};
+   
+
   //telas 
   let telaAtiva = {}; 
   function mudaParaTela(novaTela){ 
-    telaAtiva = novaTela; 
+    telaAtiva = novaTela;
+    
+    if(telaAtiva.inicializa){
+      telaAtiva.inicializa()
+    }
 
   }
 
   const telas = {
     INICIO: {
+      inicializa(){
+        globais.flappyBird = criaFlappyBird(); 
+      }, 
       desenha(){
         planoDeFundo.desenha(); 
       chao.desenha();  
-      flappyBird.desenha(); 
+      globais.flappyBird.desenha(); 
         mensagemGetReady.desenha();
         
       },
@@ -138,10 +182,13 @@ const planoDeFundo = {
     desenha(){
       planoDeFundo.desenha(); 
       chao.desenha();  
-      flappyBird.desenha(); 
+      globais.flappyBird.desenha(); 
+    },
+    click(){
+      globais.flappyBird.pula(); 
     },
     atualiza(){
-      flappyBird.atualiza();
+      globais.flappyBird.atualiza();
     }
   }
 
